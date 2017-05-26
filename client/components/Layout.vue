1267f5f87a3463afbfab9014b3fba7a28b9d30f9
NewsCartDev <newscart.dev@gmail.com>
2017-05-26 14:48:51 -0500

    Add layout logic to vuex

--- client/components/Layout.vue
+++ client/components/Layout.vue
@@ -6,7 +6,7 @@
               <h3>Layout</h3>
           </header>
           <div class="nc-inner-container nc-flex">
-            <div class="nc-layout-card" @click="handleLayoutClick(index)" v-for="(layout, index) in layouts">
+            <div class="nc-layout-card" @click="handleLayoutClick(index)" v-for="(layout, index) in possibleLayouts">
               <figure>
                 <img :src="layout.imageURL" :class="{'nc-layout-card--selected': layout.selected}"/>
                 <caption>{{layout.type}}</caption>
@@ -18,59 +18,19 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import Navigation from './Navigation.vue'
 export default {
   components: {
     Navigation
   },
-  data() {
-    return {
-      layouts: [
-        {
-          type: 'basic',
-          imageURL: "https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/basic-template.png?alt=media&token=5bf911c9-a5e6-49eb-952f-99a97d21ea9c",
-          selected: false
-        },
-        {
-          type: '1 2 1',
-          imageURL: "https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/1-2-1-template.png?alt=media&token=49f03fd3-92c4-42ab-82cd-a573adc61e7f",
-          selected: false
-        },
-        {
-          type: '1 1 2',
-          imageURL: "https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/1-1-2-template.png?alt=media&token=0ed4d797-79ac-44f1-b71a-165318825b93",
-          selected: false
-        }
-      ]
-    }
+  computed: {
+    ...mapGetters('layouts', ['possibleLayouts'])
   },
   methods: {
     handleLayoutClick(index) {
-      this.layouts[index].selected = !this.layouts[index].selected
+      this.$store.commit('layouts/TOGGLE_SELECTED_TEMPLATE', index)
     }
   }
 }
 </script>
-<style lang="scss">
-  .nc-layout-card {
-    padding: .5em;
-    &--selected {
-      border: 3px solid #28f392;
-    }
-
-    figure {
-      box-sizing: border-box;
-    }
-
-    figure img{
-      padding-bottom: 1.5em;
-    }
-
-    figure img:hover {
-      cursor: pointer;
-    }
-  }
-  caption {
-    display: block;
-  }
-</style>
---
+++ client/store/modules/layouts/index.js
@@ -0,0 +1,45 @@
+import R from 'ramda'
+
+const TOGGLE_SELECTED_TEMPLATE = 'TOGGLE_SELECTED_TEMPLATE'
+
+const getters = {
+  possibleLayouts (state) {
+    return state.layouts
+  },
+  selectedLayout (state) {
+    return state.layouts.filter(l => l.selected)
+  }
+}
+
+const mutations = {
+  [TOGGLE_SELECTED_TEMPLATE]: (state, index) => {
+    state.layouts = state.layouts.map((layout, i) => i === index ? R.set(R.lensProp('selected'), !layout.selected, layout) : layout)
+  }
+}
+
+const state = {
+  layouts: [
+    {
+      type: 'basic',
+      imageURL: 'https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/basic-template.png?alt=media&token=5bf911c9-a5e6-49eb-952f-99a97d21ea9c',
+      selected: false
+    },
+    {
+      type: '1 2 1',
+      imageURL: 'https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/1-2-1-template.png?alt=media&token=49f03fd3-92c4-42ab-82cd-a573adc61e7f',
+      selected: false
+    },
+    {
+      type: '1 1 2',
+      imageURL: 'https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/1-1-2-template.png?alt=media&token=0ed4d797-79ac-44f1-b71a-165318825b93',
+      selected: false
+    }
+  ]
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  getters
+}

924aae01f218056f161f740bddf846276d3b2549
NewsCartDev <newscart.dev@gmail.com>
2017-05-29 22:37:03 -0500

    remove politcal news sources

--- functions/libs/newsapi/libs/getSources.js
+++ functions/libs/newsapi/libs/getSources.js
@@ -2,12 +2,9 @@
 // This function allows for retrieving all English-language
 // sources from the NewsAPI
 const Promise = require('bluebird')
-// const axios = require('./axios')
-// const logger = require('../../logger')
 
 module.exports = function getSources() {
   const whiteListedSites = [
-    'the-new-york-times',
     'recode',
     'techcrunch',
     'the-verge',
@@ -16,14 +13,4 @@ module.exports = function getSources() {
   return new Promise(function (resolve, reject)  {
       return resolve(whiteListedSites)
   })
-  // return axios.get('/sources?language=en')
-  //         .then(function handleGetSourcesRes(getSourcesRes) {
-  //           return getSourcesRes.data.sources
-  //         })
-  //         .catch(function handleGetSourcesErr(getSourcesErr) {
-  //           /* eslint-disable */
-  //           logger.trace('Error Retrieving Sources from NewsAPI:' + getSourcesErr)
-  //           /* eslint-enable */
-  //           return getSourcesErr
-  //         })
 }

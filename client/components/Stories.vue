c879a2a3f16be899f02e99152548c136cd52e175
Steven T. Norris <steven@fortyau.com>
2017-06-01 15:02:58 -0500

    Populate date filter. Non-functional

--- client/components/Stories.vue
+++ client/components/Stories.vue
@@ -7,14 +7,20 @@
       <div class="nc-articles__container">
         <div class="nc-articles__controls" v-if="allArticles.length > 0">
           <md-input-container>
-           <label for="selectedFilter">Select By Source</label>
-           <md-select name="selectedFilter" v-model="selectedFilter">
-              <md-option :value="group" :key="i" v-for="(group,i) in Object.keys(articlesGrouped)">{{group | idToTitle}}</md-option>
-           </md-select>
-         </md-input-container>
+             <label for="sourceFilter">Select By Source</label>
+             <md-select name="sourceFilter" v-model="sourceFilter">
+                <md-option :value="group" :key="i" v-for="(group,i) in Object.keys(articlesSourceGrouped)">{{group | idToTitle}}</md-option>
+             </md-select>
+          </md-input-container>
+          <md-input-container>
+              <label for="dateFilter">Select By Date</label>
+              <md-select name="dateFilter" v-model="dateFilter">
+                 <md-option :value="filterDate" :key="i" v-for="(filterDate,i) in sortedFilterDate(articlesFilterDateGrouped)">{{filterDate | idToTitle}}</md-option>
+              </md-select>
+          </md-input-container>
         </div>
         <div class="nc-articles nc-flex nc-flex--wrap">
-          <article-card v-bind:article="article" :key="article.index" v-for="article in articlesGrouped[selectedFilter]"></article-card>
+          <article-card v-bind:article="article" :key="article.index" v-for="article in articlesSourceGrouped[sourceFilter]"></article-card>
         </div>
       </div>
     </section>
@@ -27,14 +33,45 @@
   import Navigation from './Navigation.vue'
   import ArticleCard from './ArticleCard.vue'
   export default {
-    created() {
+    beforeMount() {
       cloudFunctions.get('/getNews')
       .then(articles => {
         return this.$store.commit('articles/RECEIVE_ARTICLES', articles)
       })
     },
     computed: {
-    ...mapGetters('articles', ['allArticles', 'articles', 'articlesGrouped'])
+    ...mapGetters('articles', ['allArticles', 'articles', 'articlesSourceGrouped', 'articlesFilterDateGrouped'])
+    },
+    methods: {
+      sortedFilterDate:  function (articles) {
+        var thing = Object.keys(articles).sort(function(a,b) {
+            var dateA = new Date(a)
+            var dateB = new Date(b)
+            if(a == "all"){
+              return false
+            }
+            else if(dateA == null || isNaN(dateA)){
+              return false
+            }
+            else if(dateB == null || isNaN(dateB)){
+              return true
+            }
+            else {
+              return dateA < dateB
+            }
+          }
+        ).map(function(dateS){
+          var prefixD = dateS.substring(0, dateS.indexOf('T'))
+          if(prefixD.trim() == ""){
+            return dateS
+          }
+          else{
+            var pieces = prefixD.split('-')
+            return pieces[1] + "/" + pieces[2] + "/" + pieces[0]
+          }
+        })
+        return thing
+      }
     },
     components: {
       Navigation,
@@ -42,7 +79,8 @@
     },
     data() {
       return {
-        selectedFilter: 'all'
+        sourceFilter: 'all',
+        dateFilter: 'all'
       }
     },
     filters: {

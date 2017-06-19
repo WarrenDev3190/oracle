3be3e8499b03ebb98df5424267bebfa11adf6c1f
Steven T. Norris <steven@fortyau.com>
2017-06-18 20:10:12 -0500

    -Completed Spotlight editor

---
+++ client/components/editors/SpotlightEditor.vue
@@ -0,0 +1,60 @@
+<template lang="html">
+
+  <div class="nc-edit">
+    <editor-title :title="'Header'" />
+    <image-editor :title="'Image'" v-model="image" @input="updateInput" />
+    <html-editor :title="'Text'" v-model="bodyHtml" @input="updateInput" />
+  </div>
+
+</template>
+<script type="text/javascript">
+  import EditorTitle from './EditorTitle.vue'
+  import ImageEditor from './ImageEditor.vue'
+  import HtmlEditor from './HtmlEditor.vue'
+  export default {
+    components: {
+      EditorTitle,
+      ImageEditor,
+      HtmlEditor
+    },
+    computed: {
+    },
+    watch:{
+      value(newValue){
+        this.titleText = newValue.titleText
+        this.image = newValue.image
+        this.bodyHtml = newValue.bodyHtml
+      }
+    },
+    methods: {
+      updateInput: function(value){
+        this.$emit('input', {
+          titleText: this.titleText,
+          image: this.image,
+          bodyHtml: this.bodyHtml
+        })
+      }
+    },
+    data: function(){
+      return {
+        titleText: this.value.titleText,
+        image: this.value.image,
+        bodyHtml: this.value.bodyHtml
+      }
+    },
+    props: {
+      value: {
+        default: {
+          titleText: "",
+          image: "",
+          bodyHtml: ""
+        },
+        type: Object
+      },
+      editorId: {
+        default: "SpotlightEditor",
+        type: String
+      }
+    }
+  }
+</script>
--- client/components/editors/index.js
+++ client/components/editors/index.js
@@ -3,11 +3,13 @@ import HiresEditor from './HiresEditor.vue'
 import JobsEditor from './JobsEditor.vue'
 import NewsEditor from './NewsEditor.vue'
 import HeroEditor from './HeroEditor.vue'
+import SpotlightEditor from './SpotlightEditor.vue'
 
 export default {
   FootEditor,
   HiresEditor,
   JobsEditor,
   NewsEditor,
-  HeroEditor
+  HeroEditor,
+  SpotlightEditor
 }

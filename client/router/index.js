5c5051355243b4fd2c91200f99a7dc225385d163
Steven T. Norris <steven@fortyau.com>
2017-06-21 17:33:35 -0500

    -Next a previous navigation added

--- client/router/index.js
+++ client/router/index.js
@@ -16,42 +16,84 @@ const router = new Router({
     {
       name: 'login',
       path: '/',
-      component: Login
+      component: Login,
+      meta: {
+        order: undefined
+      }
     },
     {
       name: 'topics',
       path: '/topics',
-      component: Topics
+      component: Topics,
+      meta: {
+        order: 0
+      }
     },
     {
       name: 'keywords',
       path: '/keywords',
-      component: Keywords
+      component: Keywords,
+      meta: {
+        order: 1
+      }
     },
     {
       name: 'stories',
       path: '/stories',
-      component: Stories
+      component: Stories,
+      meta: {
+        order: 2
+      }
     },
     {
       name: 'layout',
       path: '/layout',
-      component: Layout
+      component: Layout,
+      meta: {
+        order: 3
+      }
     },
     {
       name: 'not-found',
       path: '*',
-      component: NotFound
+      component: NotFound,
+      meta: {
+        order: undefined
+      }
     }
   ]
 })
 
 // mount beforeEach method
 router.beforeEach((to, from, next) => {
-  if (to.path !== '/') {
-    return store.state.user.user ? next() : next({ path: '/' })
+  if (to.path !== '/' && !store.state.user.user) {
+    return next({ path: '/' })
   }
-  next()
+  else if (to.path == '/next'){
+    var found = router.options.routes.filter(function(route){
+      return route.meta.order == from.meta.order + 1
+    })
+    if(found.length > 0){
+      return next({path: found[0].path})
+    }
+    else{
+      return next(false)
+    }
+  }
+  else if (to.path == '/previous'){
+    var found = router.options.routes.filter(function(route){
+      return route.meta.order == from.meta.order - 1
+    })
+    if(found.length > 0){
+      return next({path: found[0].path})
+    }
+    else{
+      return next(false)
+    }
+  }
+  else{
+    return next()
+  }
 })
 
 export default router

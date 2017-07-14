222a6e98d6bd8aada955fb183d9a1e93357f1e86
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2017-07-13 22:39:51 -0500

    add recommeneded flag and template builder hover styling

--- client/components/ArticleCard.vue
+++ client/components/ArticleCard.vue
@@ -3,6 +3,7 @@
        :class="{'nc-article-card--selected': article.selected}"
        :style="{'background-image': `url(${article.urlToImage})`}"
        @click="handleArticleClick">
+       <img src="../../static/imgs/newscart-icon.png" alt="recommended" v-if="isRecommended" class="nc-recommended-flag">
        <div class="nc-article-card__contents">
          <h3><a :href="article.url" target="_blank">{{article.title}}</a></h3>
          <section class="nc-article-card__details nc-flex">
@@ -14,6 +15,11 @@
 <script>
 export default {
   props: ['article'],
+  computed:{
+    isRecommended: function(article) {
+    return this.article.hasOwnProperty("recommended") && this.article.recommended;
+    }
+  },
   methods: {
     handleArticleClick() {
       this.$store.commit('articles/TOGGLE_ARTICLE_SELECT', this.article.index)
--- client/utils/index.js
+++ client/utils/index.js
@@ -2,7 +2,7 @@ import R from 'ramda'
 
 /**
  * [capitalize description]
- * @type {[type]}
+ * @type {String}
  */
 export const capitalize = R.compose(
   R.join(''),
@@ -30,37 +30,32 @@ export const isUpdateMutation = R.compose(
 )
 
 export const getMonthName = (monthNum) => {
-  var monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"]
+  var monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December']
 
   return monthNames[monthNum]
-
 }
 
 export const currentDateString = () => {
+  var d = new Date()
+  var day = d.getDate()
+  var month = getMonthName(d.getMonth())
+  var year = d.getFullYear()
 
-    var d = new Date()
-    var day = d.getDate()
-    var month = getMonthName(d.getMonth())
-    var year = d.getFullYear()
-
-    return month + " " + day + ", " + year
-
+  return month + ' ' + day + ', ' + year
 }
 
 export const monthDayString = (dateS) => {
-
-  var dateSNoT = dateS.substring(0, dateS.indexOf("T"))
-  var datePieces = dateSNoT.split("-")
+  var dateSNoT = dateS.substring(0, dateS.indexOf('T'))
+  var datePieces = dateSNoT.split('-')
   var month = getMonthName(parseInt(datePieces[1]))
   var day = datePieces[2]
 
-  return month + " " + day
-
+  return month + ' ' + day
 }
 
 export const newsSourceString = (source) => {
-  return source.replace(/-/g, " ").replace(/\b\w/g, l => l.toUpperCase())
+  return source.replace(/-/g, ' ').replace(/\b\w/g, l => l.toUpperCase())
 }
 
 /**

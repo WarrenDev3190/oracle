ffbf4638b7dd0c38caa1d419c0b33410327376b0
Steven T. Norris <steven@fortyau.com>
2017-07-17 14:22:56 -0500

    Fix date filter to return numeric values

--- client/components/Stories.vue
+++ client/components/Stories.vue
@@ -55,16 +55,19 @@
             var dateA = new Date(a)
             var dateB = new Date(b)
             if(a == "all"){
-              return false
+              return -1
             }
             else if(dateA == null || isNaN(dateA)){
-              return false
+              return 1
             }
             else if(dateB == null || isNaN(dateB)){
-              return true
+              return -1
+            }
+            else if(dateA.getTime() < dateB.getTime()){
+              return 1
             }
             else {
-              return dateA < dateB
+              return -1
             }
           }
         ).map(function(dateS){

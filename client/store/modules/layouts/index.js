d96ac41d1d257e3755e3cafab7bde1aa3dfda566
Steven T. Norris <steven@fortyau.com>
2017-07-19 15:37:04 -0500

    -Added new images for default 300x200 image, header image, and layout image -Added width constraint to layout images for scaling -Adjusted event image sizes to avoid compressing image (note, this increased width of layout to 680px instead of 600px when events section is selected)

--- client/store/modules/layouts/index.js
+++ client/store/modules/layouts/index.js
@@ -29,8 +29,8 @@ const mutations = {
 const state = {
   layouts: [
     {
-      type: 'Template 1',
-      imageURL: "https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/template1.jpg?alt=media&token=489403a9-94e6-4283-9334-c3084157d1ec",
+      type: 'Dynamic Template',
+      imageURL: "https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/Dynamic%20Template.png?alt=media&token=e8d4f318-28f9-42ec-b5d6-f72852163a7d",
       selected: true,
       component:"time-template",
       template: {
@@ -45,7 +45,7 @@ const state = {
             editable: true,
             toggleable: false,
             data:{
-              logo: 'https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/hero_placeholder.jpg?alt=media&token=c45699a1-d4c0-44b6-a948-3fe9bf8d1483',
+              logo: 'https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/600%20x%20180%20(1).png?alt=media&token=3c4b364b-a782-4345-8b67-45416e3b459d',
               heroBackground: "#FFFFFF"
             }
           },
@@ -63,7 +63,7 @@ const state = {
                         "interdum, ex sed euismod maximus, orci elit posuere erat, vel pretium mauris justo dignissim " +
                         "nisl. Crassut aliquet purus. Aenean tincidunt mi ut elementum faucibus. " +
                         "<strong><a target='_blank' href='http://www.google.com' >Link to Google </a></strong>",
-              image: "https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/placeholder.jpg?alt=media&token=07a9d840-3f5b-4a31-b47c-c0cc46f788a7"
+              image: "https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/300%20x%20200.png?alt=media&token=c3bd2155-1d60-4745-b7dc-152b516066d0"
             }
           },
           news: {
@@ -95,7 +95,7 @@ const state = {
                                     "Suspendisse viverra nisi quam. Vestibulum iaculis est vitae mauris tincidunt euismod.",
                       "url": "https://www.google.com",
                       "urlAlt": "https://www.google.com",
-                      "urlToImage": "https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/placeholder.jpg?alt=media&token=07a9d840-3f5b-4a31-b47c-c0cc46f788a7",
+                      "urlToImage": "https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/300%20x%20200.png?alt=media&token=c3bd2155-1d60-4745-b7dc-152b516066d0",
                       "publishedAt": "1990-01-01T00:00:00Z",
                       "source": "fake-news-source",
                       "index": -1,
@@ -119,7 +119,7 @@ const state = {
                                     "Suspendisse viverra nisi quam. Vestibulum iaculis est vitae mauris tincidunt euismod.",
                       "url": "https://www.google.com",
                       "urlAlt": "https://www.google.com",
-                      "urlToImage": "https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/placeholder.jpg?alt=media&token=07a9d840-3f5b-4a31-b47c-c0cc46f788a7",
+                      "urlToImage": "https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/300%20x%20200.png?alt=media&token=c3bd2155-1d60-4745-b7dc-152b516066d0",
                       "publishedAt": "1990-01-01T00:00:00Z",
                       "source": "fake-news-source",
                       "index": -1,
@@ -148,7 +148,7 @@ const state = {
                                     "Suspendisse viverra nisi quam. Vestibulum iaculis est vitae mauris tincidunt euismod.",
                       "url": "https://www.google.com",
                       "urlAlt": "https://www.google.com",
-                      "urlToImage": "https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/placeholder.jpg?alt=media&token=07a9d840-3f5b-4a31-b47c-c0cc46f788a7",
+                      "urlToImage": "https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/300%20x%20200.png?alt=media&token=c3bd2155-1d60-4745-b7dc-152b516066d0",
                       "publishedAt": "1990-01-01T00:00:00Z",
                       "source": "fake-news-source",
                       "index": -1,
@@ -168,7 +168,7 @@ const state = {
               titleText: "what's happening",
               events: [
                 {
-                  image:"https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/placeholder.jpg?alt=media&token=07a9d840-3f5b-4a31-b47c-c0cc46f788a7",
+                  image:"https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/300%20x%20200.png?alt=media&token=c3bd2155-1d60-4745-b7dc-152b516066d0",
                   title:"Event Title 1",
                   date:"Monday, January 1st",
                   description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tristique orci efficitur bibendum dapibus.",
@@ -176,7 +176,7 @@ const state = {
                   linkText:"Event Link"
                 },
                 {
-                  image:"https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/placeholder.jpg?alt=media&token=07a9d840-3f5b-4a31-b47c-c0cc46f788a7",
+                  image:"https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/300%20x%20200.png?alt=media&token=c3bd2155-1d60-4745-b7dc-152b516066d0",
                   title:"Event Title 2",
                   date:"Monday, January 1st",
                   description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tristique orci efficitur bibendum dapibus.",

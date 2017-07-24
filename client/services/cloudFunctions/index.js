df2df70c32fb9b0dfb7fea1ba950933046c1f0c8
Steven T. Norris <steven@fortyau.com>
2017-07-24 14:49:22 -0500

    -Add dev environment.

--- client/services/cloudFunctions/index.js
+++ client/services/cloudFunctions/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
+//Local Url
 //baseURL: 'http://localhost:5003/projectoracle-b9c0e/us-central1'
+//Dev Url
+//baseURL: 'https://us-central1-projectoracle-b9c0e.cloudfunctions.net'
+//Prod Url
 //baseURL: 'https://us-central1-projectoracle-b9c0e.cloudfunctions.net'
 const cloudfunctions = axios.create({
   baseURL: 'https://us-central1-projectoracle-b9c0e.cloudfunctions.net'

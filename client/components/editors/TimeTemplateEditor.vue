4a93822da3b1342e93dddcef67fff38a93f216da
Steven T. Norris <steven@fortyau.com>
2017-08-07 15:41:00 -0500

    -Save new template functionality

--- client/components/editors/TimeTemplateEditor.vue
+++ client/components/editors/TimeTemplateEditor.vue
@@ -2,21 +2,48 @@
 
   <div class="nc-edit">
     <editor-title :title="title" />
+      <div class="nc-edit__template">
+      <div class="nc-edit__template__column nc-edit__template__button" >Colors Settings</div>
+      
+      <div ref="saveDialog" class="nc-edit__template__column">
+        <div class="nc-edit__template__button nc-edit__template__save" @click="toggleDialog" v-click-outside="hideDialog">Save</div>
+        <div v-if="showSaveDialog" class="nc-edit__template__popover">
+          <div class="nc-edit__template__popover__triangle"></div>
+          <div class="nc-edit__template__popover__body">
+            <button v-if="false" class="nc-edit__template__popover__save-button nc-edit__template__popover__new-section">Save Current Template</button>
+            <div class="nc-edit__template__popover__text">Save Template as...</div>
+            <input type="text" class="nc-edit__template__popover__text-input"/>
+            <button class="nc-edit__template__popover__save-button" @click="saveNewTemplate">Save As New Template</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
     <color-editor :title="'Primary Color'" v-model="sectionBarColorStart" @input="updateInput"/>
     <color-editor :title="'Secondary Color'" v-model="sectionBarColorEnd" @input="updateInput"/>
     <color-editor :title="'Link Color'" v-model="accentColor" @input="updateInput"/>
   </div>
 
 </template>
-<script type="text/javascript">
+ <script type="text/javascript">
+  import { mapGetters } from 'vuex'
   import EditorTitle from './EditorTitle.vue'
   import ColorEditor from './ColorEditor.vue'
+  import ClickOutside from 'vue-click-outside'
+  import { db } from '../../constants/fb'
   export default {
+    mounted() {
+      this.popupItem = this.$refs["saveDialog"]
+    },
+    directives: {
+      ClickOutside
+    },
     components: {
       EditorTitle,
       ColorEditor
     },
     computed: {
+      ... mapGetters('layouts', ['selectedLayout'])
     },
     watch:{
       value(newValue){
@@ -34,6 +61,18 @@
           sectionBarColorEnd: this.sectionBarColorEnd,
           sections: this.sections
         })
+      },
+      toggleDialog: function(){
+        this.showSaveDialog = !this.showSaveDialog
+      },
+      hideDialog: function(){
+        this.showSaveDialog = false
+      },
+      saveNewTemplate: function() {
+        db.ref(`users/${this.$store.state.user.user.uid}/templates/${this.selectedLayout[0].type}`).push(this.selectedLayout[0].template)
+      },
+      saveCurrentTemplate: function() {
+        
       }
     },
     data: function(){
@@ -41,7 +80,8 @@
         accentColor: this.value.accentColor,
         sectionBarColorStart: this.value.sectionBarColorStart,
         sectionBarColorEnd: this.value.sectionBarColorEnd,
-        sections: this.value.sections
+        sections: this.value.sections,
+        showSaveDialog: false
       }
     },
     props: {

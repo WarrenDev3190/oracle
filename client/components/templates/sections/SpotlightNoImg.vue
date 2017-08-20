4fe90bca16eb5c6eab3c4115a69a8360daf67aaf
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2017-08-20 17:59:44 -0500

    Spotlight section with image removed

---
+++ client/components/templates/sections/SpotlightNoImg.vue
@@ -0,0 +1,66 @@
+<template lang="html">
+
+  <div class="nc-spotlight">
+
+    <section-title
+      :style="{
+          'margin-left':'30px',
+          'margin-right':'30px',
+          'overflow-x':'hidden',
+          'max-width':'540px'
+        }"
+      :barColorStart="barColorStart"
+      :barColorEnd="barColorEnd"
+      :titleText="titleText"
+      />
+
+    <table :style="{
+        'font-family':'Arial,Helvetica,sans-serif',
+        'font-size':'11px'
+      }"
+    >
+      <tr>
+        <td valign="top">
+          <div :style="{
+            'padding-right':'30px',
+            'padding-left':'30px',
+              'line-height':'1.5'
+            }"
+            v-html="bodyHtml"
+          >
+          </div>
+        </td>
+      </tr>
+    </table>
+
+  </div>
+
+</template>
+<script type="text/javascript">
+  import SectionTitle from "./SectionTitle.vue"
+  export default {
+    components: {
+      SectionTitle
+    },
+    methods: {
+    },
+    props: {
+      barColorStart:{
+        default:'blue',
+        type:String
+      },
+      barColorEnd:{
+        default:'black',
+        type:String
+      },
+      titleText:{
+        default:'spotlight',
+        type:String
+      },
+      bodyHtml:{
+        default:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus posuere est quis purus aliquet aliquet. Donec eget erat lobortis, porttitor urna eu, pulvinar leo. Nam risus odio, feugiat id lacinia non, ullamcorper in ante. Mauris cursus quis purus a tempor. Duis sit amet velit sapien. In turpis risus, mattis ac justo auctor, luctus pulvinar tellus. Pellentesque interdum, ex sed euismod maximus, orci elit posuere erat, vel pretium mauris justo dignissim nisl. Cras ut aliquet purus. Aenean tincidunt mi ut elementum faucibus. <strong><a href='http://www.google.com' >Link to Google </a></strong>",
+        type:String
+      }
+    }
+  }
+</script>

3a2887747dda63589254e80538aa63a58314ce39
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2017-08-22 14:56:20 -0500

    add imageOn logic for event editing

--- client/components/editors/EventsEditor.vue
+++ client/components/editors/EventsEditor.vue
@@ -2,13 +2,6 @@
 
   <div class="nc-edit">
     <editor-title :title="'Events'" />
-    <div class="nc-design__section__container">
-      <input class="nc-design__section__checkbox" id="eventImageToggle" type="checkbox" v-model="selectedLayout()[0].template.sections.events.imageOn"/>
-      <label class="nc-design__section__title hoverable" for="eventImageToggle">
-        <span v-if="checkEventsImage">Images On</span>
-        <span v-else>Images Off</span>
-      </label>
-    </div>
 
     <line-editor :title="'Title'" v-model="titleText" @input="updateInput" />
 
@@ -17,6 +10,13 @@
     </div>
 
     <div class="nc-edit__group" v-for="(event, index) in events">
+      <div class="nc-design__section__container">
+        <input class="nc-design__section__checkbox" :id="'eventImageToggle-' + index" type="checkbox" v-model="events[index].imageOn"/>
+        <label class="nc-design__section__title hoverable" :for="'eventImageToggle-' + index">
+          <span >Images On</span>
+
+        </label>
+      </div>
       <event-editor class="nc-edit__group__left" v-model="events[index]" :editorId="'EventEditor-' + index" />
       <button class="nc-edit__group__right nc-edit__remove-button" type="button" @click="removeEvent(index)" >X</button>
     </div>
@@ -36,7 +36,7 @@
     },
     computed: {
       checkEventsImage: function () {
-       return this.selectedLayout()[0].template.sections.events.imageOn
+      //  return this.selectedLayout()[0].template.sections.events.imageOn
       }
     },
     watch:{
@@ -55,6 +55,7 @@
       addEvent: function(){
         this.events.push({
           image:"https://firebasestorage.googleapis.com/v0/b/projectoracle-b9c0e.appspot.com/o/245%20x%20150.png?alt=media&token=9a72f27f-0478-4857-a237-f2585f19a36a",
+          imageOn: true,
           title:"New Event",
           date:"Monday, January 1st",
           description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tristique orci efficitur bibendum dapibus.",

6408da4dcd75bccbeb3f4287f00146ab02431871
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2018-02-21 20:40:15 -0600

    add basic list layout

--- client/components/Contacts.vue
+++ client/components/Contacts.vue
@@ -1,11 +1,38 @@
 <template lang="html">
   <div class="">
-    <h2>hello world</h2>
+    <Navigation/>
+    <div class="nc-contacts__page nc-flex">
+      <div class="nc-contacts__wrapper">
+        <div class="nc-contacts__button-wrapper nc-flex">
+        <md-button class="md-raised md-primary nc-button--primary">Create List</md-button>
+        </div>
+        <h3 class="nc-contacts__title">My Lists</h3>
+        <!-- List Preview -->
+          <div class="nc-contacts__preview-wrapper nc-flex" v-for="(list, index) in lists">
+            <p class="nc-contacts__preview">{{list.name}}</p>
+            <p class="nc-contacts__preview">{{list.contacts}} contacts</p>
+            <div class="nc-contacts__preview__actions-wrapper nc-flex">
+              <i class="material-icons nc-contacts__icon-edit">create</i>
+              <i class="material-icons nc-contacts__icon-trash">delete</i>
+            </div>
+          </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
+import Navigation from './Navigation.vue'
 export default {
+  components: {
+    Navigation
+  },
+  data(){
+    return {
+      lists: [{name: 'Sales', contacts: 20},{name: 'Engineering', contacts: 20},{name: 'Product Marketing', contacts: 20}]
+    }
+  }
 
 }
 </script>
